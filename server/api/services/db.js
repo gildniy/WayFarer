082c1a84e700e70c638076af46d8e7c1a756d7c4
Gildas Niyigena <gildniy05@gmail.com>
2019-08-13 01:15:26 +0200

    feature(db) Create users table [Finished #167846549]

--- server/api/services/db.js
+++ server/api/services/db.js
@@ -1,11 +1,53 @@
-const { Pool } = require('pg');
-
 const dotenv = require('dotenv');
+
 dotenv.config();
+
+const { Pool } = require('pg');
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 pool.on('connect', () => console.log('connected to the Database'));
+
+const createUserTable = () => {
+  const queryText = `CREATE TABLE IF NOT EXISTS
+        users(
+          id UUID PRIMARY KEY,
+          first_name VARCHAR(128) NOT NULL,
+          last_name VARCHAR(128) NOT NULL,
+          email VARCHAR(128) NOT NULL,
+          password VARCHAR(128) NOT NULL
+        )`;
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+const dropUserTable = () => {
+  const queryText = 'DROP TABLE IF EXISTS users returning *';
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+const createAllTables = () => {
+  createUserTable();
+};
+
+const deleteAllTables = () => {
+  dropUserTable();
+};
 
 pool.on('remove', () => {
   console.log('client removed');
@@ -13,6 +55,11 @@ pool.on('remove', () => {
 });
 
 module.exports = {
+  createUserTable,
+  createAllTables,
+  //
+  dropUserTable,
+  deleteAllTables,
   pool,
 };
 
