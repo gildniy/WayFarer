c0b16e57d80f7ea3f963b63f70e3980d47783296
Gildas Niyigena <gildniy05@gmail.com>
2019-08-13 17:09:44 +0200

    feature(db) Signup a user [Finished #167861505]

---
+++ server/api/db/index.js
@@ -0,0 +1,13 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const query = (text, params) => {
+  return new Promise((resolve, reject) => {
+    pool.query(text, params, (error, results) =>
+      error ? reject(error) : resolve(results.rows)
+    );
+  });
+};
